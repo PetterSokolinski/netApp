ddbbe2f2fff69876e2100b8269e22acf5a452d46
PetterSokolinski <pettersokolinski@gmail.com>
2019-03-10 19:48:57 +0100

    add routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react'
 import Login from './Components/Login'
+import Register from './Components/Register'
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
+
 
 class App extends Component {
   render() {
     return (
-      <Login />
-    );
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Login} />
+          <Route exact path="/register" component={Register} />
+        </Switch>
+      </Router>
+    )
   }
 }
 
-export default App;
+export default App
